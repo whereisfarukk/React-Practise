e52d9301fdd4715496c0586da15975fbc4f3e07f
whereisfarukk <whereisfarukk@gmail.com>
2025-01-09 17:25:03 +0600

    feat:slight change in response

--- src/components/Benefit/components/BenefitCards.jsx
+++ src/components/Benefit/components/BenefitCards.jsx
@@ -7,17 +7,20 @@ const BenefitCards = () => {
         <Icon />
       </div>
       <h4>A Legacy of Excellence</h4>
-      <div
+      {/* <div
+        clas
         style={{
+          fontSize: "1.23vw",
           fontWeight: "400",
           lineHeight: "160%",
         }}
-      >
+      > */}
+      <p>
         With over 100 completed projects and a reputation for exceptional
         craftsmanship, The Makaan Builders has become a trusted name in the
         industry. Our commitment to quality and customer satisfaction is evident
         in every home we build.
-      </div>
+      </p>
     </div>
   );
 };
